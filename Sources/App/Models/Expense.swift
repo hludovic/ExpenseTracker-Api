d87addfd4a87d66689573f7a4ec6c7a40a0181e1
Ludovic HENRY <badsteam@hotmail.com>
2023-07-28 11:55:29 -0400

    Expense model

---
+++ Sources/App/Models/Expense.swift
@@ -0,0 +1,52 @@
+//
+//  Expense.swift
+//  
+//
+//  Created by Ludovic HENRY on 27/07/2023.
+//
+
+import Fluent
+import Vapor
+
+final class Expense: Model, Content {
+    static let schema = "expenses"
+
+    @ID(key: .id)
+    var id: UUID?
+
+    @Parent(key: "categories_id")
+    var category: Category
+
+    @Parent(key: "accounts_id")
+    var account: Account
+
+    @OptionalField(key: "description")
+    var description: String?
+
+    @Field(key: "amount")
+    var amount: Int
+
+    @Field(key: "currency")
+    var currency: String
+
+    @Field(key: "date")
+    var date: Date
+
+    init() { }
+
+    init(id: UUID? = nil,
+         categoryID: Category.IDValue,
+         accountID: Account.IDValue,
+         description: String?,
+         date: Date,
+         amount: Int,
+         currency: String) {
+        self.id = id
+        self.$category.id = categoryID
+        self.$account.id = accountID
+        self.description = description
+        self.amount = amount
+        self.currency = currency
+        self.date = date
+    }
+}
