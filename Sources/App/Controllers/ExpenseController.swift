ce4aa19ea4e7ffe3eb37637348d2f142bd33c6d9
Ludovic HENRY <badsteam@hotmail.com>
2023-08-03 23:25:41 -0400

    ExpenseController added

---
+++ Sources/App/Controllers/ExpenseController.swift
@@ -0,0 +1,54 @@
+//
+//  ExpenseController.swift
+//  
+//
+//  Created by Ludovic HENRY on 30/07/2023.
+//
+
+import Fluent
+import Vapor
+
+struct ExpenseController: RouteCollection {
+    func boot(routes: RoutesBuilder) throws {
+        let expenses = routes.grouped("expenses")
+        expenses.get(use: index)
+        expenses.post(use: create)
+        expenses.put(use: update)
+        expenses.delete(":expenseID", use: delete)
+    }
+
+    func index(req: Request) async throws -> [Expense] {
+        try await Expense.query(on: req.db).all()
+    }
+
+    func create(req: Request) async throws -> Expense {
+        let expense = try req.content.decode(Expense.self)
+        try await expense.save(on: req.db)
+        return expense
+    }
+
+    func update(req: Request) async throws -> HTTPStatus {
+        let expense = try req.content.decode(Expense.self)
+        guard let expenseFromDB = try await Expense.find(expense.id, on: req.db) else {
+            throw Abort(.notFound)
+        }
+        expenseFromDB.account = expense.account
+        expenseFromDB.description = expense.description
+        expenseFromDB.amount = expense.amount
+        expenseFromDB.currency = expense.currency
+        expenseFromDB.date = expense.date
+        try await expenseFromDB.update(on: req.db)
+        return .ok
+    }
+
+    func delete(req: Request) async throws -> HTTPStatus {
+        guard let expenseID = req.parameters.get("expenseID") else {
+            throw Abort(.badRequest)
+        }
+        guard let expense = try await Expense.find(UUID(uuidString: expenseID), on: req.db) else {
+            throw Abort(.notFound)
+        }
+        try await expense.delete(on: req.db)
+        return .noContent
+    }
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -13,4 +13,5 @@ func routes(_ app: Application) throws {
 
     try app.register(collection: AccountController())
     try app.register(collection: CategoryController())
+    try app.register(collection: ExpenseController())
 }
